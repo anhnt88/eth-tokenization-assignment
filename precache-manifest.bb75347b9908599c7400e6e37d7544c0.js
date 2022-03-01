97347d9a88d31059374a6f4b7498a86e00cb07fc
Nguyễn Tuấn Anh 2 <anhnt.ktmt@gmail.com>
2022-03-01 21:27:35 +0700

    Updates

---
+++ precache-manifest.bb75347b9908599c7400e6e37d7544c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b54837fa01c3942ea6cfe1a4408a1e8",
+    "url": "/eth-tokenization-assignment/index.html"
+  },
+  {
+    "revision": "b1e0d873fbe733096be6",
+    "url": "/eth-tokenization-assignment/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "24cc74c53b618b6202f4",
+    "url": "/eth-tokenization-assignment/static/js/2.60854c46.chunk.js"
+  },
+  {
+    "revision": "b1e0d873fbe733096be6",
+    "url": "/eth-tokenization-assignment/static/js/main.24f1ac87.chunk.js"
+  },
+  {
+    "revision": "163a79fd1134a7716e0c",
+    "url": "/eth-tokenization-assignment/static/js/runtime-main.3eb9d4c6.js"
+  }
+]);
